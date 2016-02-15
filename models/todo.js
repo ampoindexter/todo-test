dc6b4dc804fb4027a4d33a119cb981d55c5dd074
Alicia Poindexter <alcampbel@gmail.com>
2016-02-15 10:14:41 -0800

    start routes

---
+++ models/todo.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var todoSchema = mongoose.Schema({
+  description: { type: String },
+  dueDate: { type: Date },
+  complete: { type: Boolean }
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo Test'});
+var Todo = require('../models/todo');
+
+router.get('/', function(req, res) {
+  Todo.find(function(err, todos) {
+    if(err) return res.status(400).send(err);
+    res.render('index', {title: 'Todo Test!'}).send(todos);
+  });
 });
 
 module.exports = router;
