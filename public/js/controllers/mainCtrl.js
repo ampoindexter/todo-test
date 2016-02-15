bd1fa64db21606256eeb2b210bbf3e8b3356dd77
Alicia Poindexter <alcampbel@gmail.com>
2016-02-15 11:15:54 -0800

    add main controller

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,7 +1,39 @@
 'use strict';
 
-
+var app = angular.module('MyApp');
 
 app.controller('mainCtrl', function($scope, $http) {
-  
+  $scope.formData = {};
+
+  $http.get('/todos')
+    .success(function(data) {
+      $scope.todos = data;
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+  $scope.createTodo = function() {
+    $http.post('/todos', $scope.formData)
+      .success(function(data) {
+        $scope.formData = {};
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  $scope.deleteTodo = function(id) {
+    $http.delete('/todos/' + id)
+      .success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ', + data);
+      });
+  }
 });
